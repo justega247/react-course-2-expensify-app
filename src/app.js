758e2735900603b23baf1b629cadb5f2bb563a7d
Oghenetega <okeremeta.tega@gmail.com>
2018-07-24 12:52:26 +0100

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -18,4 +19,8 @@ const jsx = (
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(jsx, appRoot);
+ReactDOM.render(<p>Loading...</p>, appRoot);
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, appRoot);
+});
